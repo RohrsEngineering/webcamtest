093f15ada4cab771f9d2d7e403db04d0efad4043
Eric Rohrs <rohrs.eric@gmail.com>
2016-11-07 17:44:07 +0000

    Editted scans.js

--- app/assets/javascripts/scans.js
+++ app/assets/javascripts/scans.js
@@ -1,44 +1,50 @@
 window.onload = function() {
-  var canvas, context, ctx, streaming, video, videoObj;
-  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
-  canvas = document.getElementById('canvas');
-  context = canvas.getContext('2d');
-  video = document.getElementById('video');
-  videoObj = {
-    video: true,
-    audio: false
-  };
-  streaming = false;
-  $('#btnStart').click(function() {
-    var localMediaStream;
-    localMediaStream = void 0;
-    if (navigator.getUserMedia) {
-      navigator.getUserMedia(videoObj, function(stream) {
-        video.src = navigator.webkitGetUserMedia ? window.URL.createObjectURL(stream) : stream;
-        localMediaStream = stream;
-        streaming = true;
-        return console.log('started streaming');
-      }, function(error) {
-        return console.error('Video capture error: ', error.code);
-      });
-      $('#btnStop').click(function() {
-        localMediaStream.getVideoTracks()[0].stop;
-        streaming = false;
-        return console.log('stopped streaming');
-      });
-      return $('#btnPhoto').click(function() {
-        if (streaming) {
-          console.log('picture taken');
-          return context.drawImage(video, 0, 0, 320, 240);
-        }
-      });
-    }
-  });
-  ctx = document.getElementById('hexnum').getContext('2d');
-  ctx.font = "48px serif";
-  if (streaming) {
-    return ctx.fillText("Hello world", 5, 50);
-  } else {
-    return ctx.fillText("Not Streaming", 5, 50);
-  }
+
+	//Compatibility
+	/*global navigator*/
+	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+
+	var canvas = document.getElementById("canvas"),
+		context = canvas.getContext("2d"),
+		video = document.getElementById("video"),
+		btnStart = document.getElementById("btnStart"),
+		btnStop = document.getElementById("btnStop"),
+		btnPhoto = document.getElementById("btnPhoto"),
+		videoObj = {
+				video: true,
+				audio: false
+		};
+	var streaming = false;
+	btnStart.addEventListener("click", function() {
+		var localMediaStream;
+		if (navigator.getUserMedia) {
+			navigator.getUserMedia(
+				videoObj,
+				function(stream) {              
+					video.src = (navigator.webkitGetUserMedia) ? window.URL.createObjectURL(stream) : stream;
+					localMediaStream = stream;
+					streaming = true;
+				},
+				function(error) {
+					console.error("Video capture error: ", error.code);
+				}
+			);
+
+			btnStop.addEventListener("click", function() {
+					localMediaStream.getVideoTracks()[0].stop();
+			});
+
+			btnPhoto.addEventListener("click", function() {
+				context.drawImage(video, 0, 0, 320, 240);
+
+			});
+		}
+	});
+	// ctx.font = "48px serif";
+	// if streaming {
+	// 	ctx.fillText("Hello world", 5, 50);
+	// }
+	// else {
+	// 	ctx.fillText("Not Streaming", 5, 50);
+	// }
 };
