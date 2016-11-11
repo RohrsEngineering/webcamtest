88637decfc7a54f98ae8d110d0520db674dc56e2
Brandon Richey <diamondgfx@gmail.com>
2016-11-11 08:20:21 -0500

    Add tests for scan controller and model, also add Spring binstubs

--- test/models/scan_test.rb
+++ test/models/scan_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class ScanTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Creates a Scan with valid data" do
+    scan = Scan.create(data: "Test One", number: 12345)
+    assert scan.is_a?(Scan)
+    assert scan.valid?
+  end
+
+  test "Does not create a Scan with no data" do
+    scan = Scan.new(data: nil, number: 12345)
+    assert_not scan.valid?
+  end
+
+  test "Does not create a Scan with no number" do
+    scan = Scan.new(data: "Example", number: nil)
+    assert_not scan.valid?
+  end
 end
