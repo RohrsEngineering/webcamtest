a06f58997e6794e8fb6efaeaac9c0943fe5f6dd8
Eric Rohrs <rohrs.eric@gmail.com>
2016-11-11 15:49:24 +0000

    Updated scans.js to jquery style with functions

--- app/assets/javascripts/scans.js
+++ app/assets/javascripts/scans.js
@@ -1,47 +1,57 @@
 $(function() {
 	if ($("#canvas").length === 0) { return; }
-	//Compatibility
-	/*global navigator*/
-	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
 
+	//Compatibility
+	/*global navigator, $, stream*/
+	navigator.getUserMedia = navigator.getUserMedia ||
+		navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+		
 	var canvas = document.getElementById("canvas"),
 		context = canvas.getContext("2d"),
-		video = document.getElementById("video"),
-		btnStart = document.getElementById("btnStart"),
-		btnStop = document.getElementById("btnStop"),
-		btnPhoto = document.getElementById("btnPhoto"),
+		video = document.querySelector('video'),
 		streaming = false,
-		videoObj = {
+		constraints = {
 				video: true,
 				audio: false
 		};
+	/*global stream*/
+	function successCallback(stream) {
+		window.stream = stream; // stream available to console
+		if (window.URL) {
+    		video.src = window.URL.createObjectURL(stream);
+		} else {
+    		video.src = stream;
+		}
+		console.log('navigator.getUserMedia active');
+	}
 
-	btnStart.addEventListener("click", function() {
-		var localMediaStream;
+	function errorCallback(error) {
+	  console.log('navigator.getUserMedia error: ', error);
+	}
+	
+	$( "#btnStart" ).click( function() {
 		if (navigator.getUserMedia) {
-			navigator.getUserMedia(
-				videoObj,
-				function(stream) {
-					video.src = (navigator.webkitGetUserMedia) ? window.URL.createObjectURL(stream) : stream;
-					localMediaStream = stream;
-					streaming = true;
-				},
-				function(error) {
-					console.error("Video capture error: ", error.code);
-				}
-			);
+			navigator.getUserMedia(constraints, successCallback, errorCallback);
+
+			$("#btnStop").click( function() {
+					stream.getVideoTracks()[0].stop();
+			});
+
+			$("#btnPhoto").click( function() {
+				snapPhoto();
+				//context.drawImage(video, 0, 0, 320, 240);
+				console.log('Photo button press');
+			});
 		}
 	});
-
-	btnStop.addEventListener("click", function() {
-			localMediaStream.getVideoTracks()[0].stop();
-	});
-
-	btnPhoto.addEventListener("click", function() {
+	
+	function snapPhoto(img) {
 		if (context) { context.drawImage(video, 0, 0, 320, 240); }
 		$("#scan_data").val("Example Data");
 		$("#scan_number").val(12345);
-	});
+		img = context.getImageData(0, 0, video.width, video.height);
+		console.log("Photo Drawn");
+	}
 	// ctx.font = "48px serif";
 	// if streaming {
 	// 	ctx.fillText("Hello world", 5, 50);
